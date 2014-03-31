c39ed550c15cced11b708f32bdca818703f694e1
Eric J. Holmes <eric@ejholmes.net>
2014-03-30 23:38:35 -0700

    Cleanup.

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -6,17 +6,5 @@ func IsLeapYear(year int) bool {
 		return year % divisor == 0
 	}
 
-	if isDivisibleBy(4) {
-		if isDivisibleBy(100) {
-			if isDivisibleBy(400) {
-				return true
-			} else {
-				return false
-			}
-		} else {
-			return true
-		}
-	} else {
-		return false
-	}
+	return isDivisibleBy(4) && !isDivisibleBy(100) || isDivisibleBy(400)
 }
