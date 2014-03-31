cd887c4dad0f2368d98752284fff04b73a1ab192
Eric J. Holmes <eric@ejholmes.net>
2014-03-30 22:29:57 -0700

    Initial commit.

---
+++ go/leap/leap.go
@@ -0,0 +1,21 @@
+package leap
+
+func IsLeapYear(year int) bool {
+	isDivisibleBy := func(divisor int) bool {
+		return year % divisor == 0
+	}
+
+	if isDivisibleBy(4) {
+		if isDivisibleBy(100) {
+			if isDivisibleBy(400) {
+				return true
+			} else {
+				return false
+			}
+		} else {
+			return true
+		}
+	} else {
+		return false
+	}
+}
